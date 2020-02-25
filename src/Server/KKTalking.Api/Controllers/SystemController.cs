5fec91684af3f735354c566a83774f5f62e3e979
Takaaki Suzuki <xin9le@live.jp>
2020-02-25 22:28:22 +0900

    ハートビート処理を追加

---
+++ src/Server/KKTalking.Api/Controllers/SystemController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading;
+using Microsoft.Azure.WebJobs;
+
+
+
+namespace KKTalking.Api.Controllers
+{
+    /// <summary>
+    /// システム全体に関する要求を処理する機能を提供します。
+    /// </summary>
+    public sealed class SystemController
+    {
+        #region コンストラクタ
+        /// <summary>
+        /// インスタンスを生成します。
+        /// </summary>
+        public SystemController()
+        { }
+        #endregion
+
+
+        /// <summary>
+        /// 定期的なハートビートを行い Hot Standby 状態を維持します。
+        /// </summary>
+        /// <param name="timer"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [Disable("Disable_AlwaysOn")]
+        [FunctionName("Timer_AlwaysOn")]
+        public void AlwaysOn(
+            [TimerTrigger("%CRON_AlwaysOn%")]TimerInfo timer,
+            CancellationToken cancellationToken)
+        {}
+    }
+}
