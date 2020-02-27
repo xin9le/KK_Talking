f01eaa62293b2bdd9ccc2171b7ca62e4455550a0
Takaaki Suzuki <xin9le@live.jp>
2020-02-28 00:00:56 +0900

    Azure Cognitive Search の構成情報を取得できるようにした

---
+++ src/Server/KKTalking.Api/AppSettings.cs
@@ -0,0 +1,25 @@
+﻿using KKTalking.Api.Configurations;
+
+
+
+namespace KKTalking.Api
+{
+    /// <summary>
+    /// アプリケーション設定を表します。
+    /// </summary>
+    public sealed class AppSettings
+    {
+#pragma warning disable CS8618
+        /// <summary>
+        /// Azure Web Jobs が利用する Storage への接続文字列を取得します。
+        /// </summary>
+        public string AzureWebJobsStorage { get; private set; }
+
+
+        /// <summary>
+        /// Azure Cognitive Service の構成を取得します。
+        /// </summary>
+        public CognitiveSearchConfiguration CognitiveSearch { get; private set; }
+#pragma warning restore CS8618
+    }
+}
---
+++ src/Server/KKTalking.Api/Configurations/CognitiveSearchConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace KKTalking.Api.Configurations
+{
+    /// <summary>
+    /// Azure Cognitive Search の構成を表します。
+    /// </summary>
+    public sealed class CognitiveSearchConfiguration
+    {
+#pragma warning disable CS8618
+        /// <summary>
+        /// サービス名を取得します。
+        /// </summary>
+        public string ServiceName { get; private set; }
+
+
+        /// <summary>
+        /// API キーを取得します。
+        /// </summary>
+        public string ApiKey { get; private set; }
+#pragma warning restore CS8618
+    }
+}
