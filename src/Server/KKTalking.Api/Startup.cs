7f532aad6608bebabca81a6d353eaea1ce8eb77d
Takaaki Suzuki <xin9le@live.jp>
2020-03-02 00:46:33 +0900

    不要な using を削除

--- src/Server/KKTalking.Api/Startup.cs
+++ src/Server/KKTalking.Api/Startup.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 
 [assembly: FunctionsStartup(typeof(KKTalking.Api.Startup))]
 
